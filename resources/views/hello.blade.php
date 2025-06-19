0b6bfb35576010a8346020fc9759d6dd979673eb
rifukii <black.spidey43@gmail.com>
2025-06-19 13:22:25 +0700

    Deploy 5 Buat Hello World!

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Halaman Hello</title>
+</head>
+<body>
+    <h1>Hello World dari Laravel!</h1>
+    <p>Selamat datang di halaman awal proyek Laravel kamu.</p>
+</body>
+</html>
