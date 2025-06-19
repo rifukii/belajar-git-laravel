8cd01b909296fda2f55bc482beaeec3da20218c7
rifukii <black.spidey43@gmail.com>
2025-06-19 13:47:26 +0700

    Deploy 6 Buat Model Persiapan Dummy Data

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Fasum;
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
 Route::get('/', function () {
-    return view('welcome');
+    $fasums = Fasum::all();
+    return view('hello', compact('fasums'));
 });
 
 Route::get('/hello', function () {
